8d21cf581641bf4d0c8fea81c7050dc154e0247f
siers <wimuan@gmail.com>
2023-10-28 00:58:19 +0300

    Split nix into default/shell

---
+++ default.nix
@@ -0,0 +1,26 @@
+{ haskellPackages }:
+
+let
+  drv = haskellPackages.callPackage ({ mkDerivation, base, brick, containers, directory, filepath
+      , lens, lib, linear, optparse-applicative, random, transformers
+      , vty, mtl, async, criterion, hspec, hspec-discover, QuickCheck
+      }:
+      mkDerivation {
+        pname = "tetris";
+        version = "0.1.4.1";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          base brick containers lens linear random transformers vty mtl async criterion hspec hspec-discover QuickCheck
+        ];
+        executableHaskellDepends = [
+          base directory filepath optparse-applicative
+        ];
+        homepage = "https://github.com/samtay/tetris#readme";
+        license = lib.licenses.bsd3;
+      }) {};
+
+  # src = fetchgit { url = "https://github.com/siers/tetris"; hash = "sha256-N8PG/bIpF/rUaqzhqBKn/z2Fjk8SVcsEpSPacCSXSRI="; };
+in
+  drv
--- shell.nix
+++ shell.nix
@@ -2,34 +2,10 @@
 
 with nixpkgs;
 
-let
-  drv = pkgs.haskellPackages.callPackage ({ mkDerivation, base, brick, containers, directory, filepath
-      , lens, lib, linear, optparse-applicative, random, transformers
-      , vty, mtl, async, criterion, hspec, hspec-discover, QuickCheck
-      }:
-      mkDerivation {
-        pname = "tetris";
-        version = "0.1.4.1";
-        src = ./.;
-        isLibrary = true;
-        isExecutable = true;
-        libraryHaskellDepends = [
-          base brick containers lens linear random transformers vty mtl async criterion hspec hspec-discover QuickCheck
-        ];
-        executableHaskellDepends = [
-          base directory filepath optparse-applicative
-        ];
-        homepage = "https://github.com/samtay/tetris#readme";
-        license = lib.licenses.bsd3;
-      }) {};
-
-  src = fetchgit { url = "https://github.com/siers/tetris"; hash = "sha256-N8PG/bIpF/rUaqzhqBKn/z2Fjk8SVcsEpSPacCSXSRI="; };
-
-in
-  mkShell {
-    inputsFrom = [ drv.env ];
-    buildInputs = [
-      haskell-language-server
-      cabal-install
-    ];
-  }
+mkShell {
+  inputsFrom = [ (callPackage (import ./default.nix) {}).env ];
+  buildInputs = [
+    haskell-language-server
+    cabal-install
+  ];
+}
